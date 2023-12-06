a6b9150e3e214798666adcd2323b68f03c0e14d6
jackmwhit <jack.whitworth@substrakt.com>
2023-12-06 14:26:04 +0000

    Added css inline variables with PHP hook for modification

--- blocks/build/text/index.asset.php
+++ blocks/build/text/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'eb258a1919da77befa46');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'df9ea01cd515745bf7f8');
